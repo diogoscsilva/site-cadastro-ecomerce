0187835b821bd13b619f7cf2daad3f7147d152e9
Diogo Santoro Costa da Silva <diogosantoro@gmail.com>
2021-07-25 21:38:21 -0300

    corrigindo img react

--- src/components/sectionLoja.js
+++ src/components/sectionLoja.js
@@ -1,22 +1,17 @@
 import React, {Image} from "react"
 import storage from "../scirpts/dataController"
-import {ironFoto} from "../assets/iron.jpg"
 
-const fotos = {
-  ironFoto,
-}
 
 export default function Loja () {
   const itensLength = storage.length('produtos')
   let intesList = []
   for (let i = 0; i < itensLength; i++) {
     let item = storage.getRow('produtos', i)
-    const foto =  <Image source={item.foto || fotos[item.produto + 'fotos']}/>
     intesList.push(
       <section className= "section">
         <div className = "card">
           <h3 className = "product-name">{item.produto}</h3>
-          {foto}
+          <Image source={"../assets/iron.jpg"}/>
           <p className = "description">
             {item.descricao}
           </p>
