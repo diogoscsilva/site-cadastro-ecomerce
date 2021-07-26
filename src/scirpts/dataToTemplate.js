2cb838965bf8b9dee533a120e0939cf4990c050a
Diogo Santoro Costa da Silva <diogosantoro@gmail.com>
2021-07-25 22:38:47 -0300

    adicionado dados precarregados e link das imagens

--- src/scirpts/dataToTemplate.js
+++ src/scirpts/dataToTemplate.js
@@ -99,10 +99,45 @@ export const buttonsText = {
 export const dataPreLoad = {
   produtos: [
     {
+      descricao: "notebook apple",
+      preco: "10.500",
+      produto: "apple",
+      foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfvXZE7HUKJKySqWof5yx1JzVrHZRdx5xp8w&usqp=CAU",
+      quantidade: 0,
+    },
+    {
       descricao: "ferro de passar",
       preco: "150",
       produto: "iron",
       foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzCoxdXgijSpFy_FiJLJgpoTA1ZMXmCa3KyA&usqp=CAU",
+      quantidade: 0,
+    },
+    {
+      descricao: "torradeira",
+      preco: "740",
+      produto: "toaster",
+      foto: "https://images-americanas.b2w.io/produtos/1526236448/imagens/torradeira-eletrica-yabano-4-fatias-estilo-retro-aco-inoxidavel-amarelo-110v/1526236448_1_large.jpg",
+      quantidade: 0,
+    },
+    {
+      descricao: "smartphone",
+      preco: "6800",
+      produto: "iphone",
+      foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJbaEA-P4dUVAXvOCCqpUH3T3QTp2AJ-eVfQ&usqp=CAU",
+      quantidade: 0,
+    },
+    {
+      descricao: "monitor ultrawwide",
+      preco: "1150",
+      produto: "monitos",
+      foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRU7onot7ovElGTOUtM5hX5-rerzAWBjxEaw&usqp=CAU",
+      quantidade: 0,
+    },
+    {
+      descricao: "barbeador eletrico",
+      preco: "180",
+      produto: "razor",
+      foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcNSdmQz1-L4NRy5fFzsGp77fVOKm1uSq2zw&usqp=CAU",
       quantidade: 0,
     },
   ],
